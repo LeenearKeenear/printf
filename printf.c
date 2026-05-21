1f484d5ac19e666ed3d4ce89465bf04e906ae2c7
Leenear <leenear@proton.me>
2026-05-21 16:25:06 +0400

    fix silent truncation

--- printf.c
+++ printf.c
@@ -46,9 +46,9 @@
 
 // 'ntoa' conversion buffer size, this must be big enough to hold one converted
 // numeric number including padded zeros (dynamically created on stack)
-// default: 32 byte
+// default: 32 byte (now supports 64 bit :> )
 #ifndef PRINTF_NTOA_BUFFER_SIZE
-#define PRINTF_NTOA_BUFFER_SIZE    32U
+#define PRINTF_NTOA_BUFFER_SIZE    64U
 #endif
 
 // 'ftoa' conversion buffer size, this must be big enough to hold one converted
